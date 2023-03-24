038712cc96f4d3aa12d04b43e6f5489e7a71c06f
Aadil Khan <aadilkhan99a@gmail.com>
2023-03-24 22:29:10 +0530

    Day 3

---
+++ datasizerange.java
@@ -0,0 +1,43 @@
+import java.lang.*;
+import java.util.*;
+
+class datasizerange
+{
+	public static void main(String arg[])
+	{
+		System.out.println("INTEGER");
+		System.out.println("Min value of Integer : "+Integer.MIN_VALUE);
+		System.out.println("Max value of Integer : "+Integer.MAX_VALUE);
+		System.out.println("Integer Byte Value : "+Integer.BYTES);
+		
+		System.out.println("FlOAT");
+		System.out.println("Min value of Float : "+Float.MIN_VALUE);
+		System.out.println("Max value of Float : "+Float.MAX_VALUE);
+		System.out.println("Float Byte Value : "+Float.BYTES);
+
+		System.out.println("BYTE");
+		System.out.println("Min value of Byte : "+Byte.MIN_VALUE);
+		System.out.println("Max value of Byte : "+Byte.MAX_VALUE);
+		System.out.println("Byte Byte Value : "+Byte.BYTES);
+
+		System.out.println("LONG");
+		System.out.println("Min value of Long : "+Long.MIN_VALUE);
+		System.out.println("Max value of Long : "+Long.MAX_VALUE);
+		System.out.println("Long Byte Value : "+Long.BYTES);
+
+		System.out.println("SHORT");
+		System.out.println("Min value of Short : "+Short.MIN_VALUE);
+		System.out.println("Max value of Short : "+Short.MAX_VALUE);
+		System.out.println("Short Byte Value : "+Short.BYTES);
+
+		System.out.println("DOUBLE");
+		System.out.println("Min value of Double : "+Double.MIN_VALUE);
+		System.out.println("Max value of Double : "+Double.MAX_VALUE);
+		System.out.println("Double Byte Value : "+Double.BYTES);
+
+		System.out.println("CHAR");
+		System.out.println("Min value of Char : "+Character.MIN_VALUE);
+		System.out.println("Max value of Char : "+Character.MAX_VALUE);
+		System.out.println("Char Byte Value : "+Character.BYTES);
+	}
+}
