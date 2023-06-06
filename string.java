afa8cd52ce7c7b3f845dca19654bce19a1cdb69d
Aadil Khan <aadilkhan99a@gmail.com>
2023-06-06 19:40:55 +0530

    String

--- string.java
+++ string.java
@@ -8,6 +8,9 @@ public class string {
         String str1 = new String(c);    //Obj created in heap
         System.out.println(str1);
 
+        String str0 = new String(str);
+        System.out.println(str0);
+
         byte b[] = {65, 66, 67, 68};
         String str2 = new String(b);    //Obj created in heap
         System.out.println(str2);
@@ -17,6 +20,11 @@ public class string {
         String str4 = "Hello world";   //Obj does not create in pool because it already created in line 6
         String str5 = new String("Hello world");  //obj is create in heap but not in pool because it already exist
 
+        String str6 = new String(c, 1, 2); // 1 = starting index, 2= number of letters
+        System.out.println(str6);
+
+        System.out.println(str == str4);  // object is same
+        System.out.println(str == str5);  // object does not same
 
     }
 }
---
+++ stringMethods.java
@@ -0,0 +1,74 @@
+import java.sql.SQLOutput;
+
+public class stringMethods {
+    public static void main(String args[]) {
+        String str = "JAVA";
+        System.out.println(str.length());
+
+        System.out.println(str.toLowerCase());
+        System.out.println(str);
+        String str1 = new String(str.toLowerCase()); // create a new object in heap
+
+        String str2 = str.toLowerCase(); // Create a new object in heap
+        str = str.toLowerCase(); // create a new object in heap and str point to new created object
+        System.out.println(str);
+
+        String str3 = "   Mr. Aadil Khan   ";
+        str3 = str3.trim();
+        System.out.println(str3);
+
+        String sub = str3.substring(3, 9); // 3= beginIndex , 9 = endingIndex(9-1) "if you give a single argument then
+        // it is take it as a beginIndex"
+        System.out.println(sub);
+
+        String rep = str3.replace('r', 's');
+        System.out.println(rep);
+
+        System.out.println(str3.startsWith("mr.")); // Check that Str3 is start with "mr." or not
+        System.out.println(str3.startsWith("Mr."));
+        System.out.println(str3.endsWith("ha")); // Check that str3 is end with "ha" or not
+        System.out.println(str3.endsWith("an"));
+
+        System.out.println(str3.charAt(6)); // return character at the index of 6
+
+        System.out.println(str3.indexOf("Aadil")); // return index number of the string "Aadil"
+
+        System.out.println(str3.indexOf("a", 7)); // search "a" from starting index = 7 (L->R)
+
+        System.out.println(str3.lastIndexOf("Khan")); // search for string "Khan" from lastindex (R->L)
+
+        // print whole string using loop
+
+        for (int i = 0; i < str3.length(); i++) {
+            System.out.print(str3.charAt(i) + " ");
+            if (i == (str3.length() - 1)) {
+                System.out.println();
+            }
+
+        }
+
+        String str4 = "Mr. Aadil Khan";
+        System.out.println(str4 == str3); // because in str3 object is changed from initaial to trimmed
+
+        System.out.println(str3.equals(str4));  // it checks that string str3 is match with str4 or not (not check that object is also same or not)
+        // case sensative
+
+        String str5 = "MR. AADIL KHAN";
+        System.out.println(str4.equalsIgnoreCase(str5));  // same as above method but case Insensative
+        String st= "python";
+        String st1= "java";
+        System.out.println(st.compareTo(st1));  // st > st1 so return =1 but 'p' is 6 times greater then 'j' (ASCII)
+                                                // if st< st1, return = -1   if st = st1, return = 0
+
+        String s = "china wall";
+        String s1 = "china tall";
+        System.out.println(s.compareTo(s1)); // china is same but 'w' is 3 times greater then 't'
+
+        System.out.println(s.contains("wall"));  // check that "wall" is present in string or not
+
+        System.out.println(s.concat(s1));  // concatinat strings s and s1
+        int x = 65;
+        String stt = String.valueOf(x);  // convert any type of data into string
+        System.out.println(stt);
+    }
+}
